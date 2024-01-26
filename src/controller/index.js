2db6200ae3a3972b571487189019f643c8ae0dac
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-01-26 22:49:06 +0530

    Task Completed

---
+++ src/controller/index.js
@@ -0,0 +1,14 @@
+const homePage = (req, res) => {
+    res.status(200).send(`
+     <div>
+     <h1>NodeJS_Hall_Booking_API_Task</h1>
+     <ul>
+        <li>Get all Rooms = endpoint: /rooms</li>
+        <li>Take a h1st of Booked rooms = endpoint: /rooms/customer</li>
+        <li>Create a new room = endpoint: /rooms/</li>
+     </ul>
+     </div>
+     `);
+  };
+  
+  export default { homePage }
