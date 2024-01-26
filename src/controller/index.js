ce42b96d355d719483a93072b4cf8b58356bc874
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-01-26 23:15:23 +0530

    Task Updated

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,10 @@
 const homePage = (req, res) => {
     res.status(200).send(`
      <div>
-     <h1>NodeJS_Hall_Booking_API_Task</h1>
+     <h1>🌟NodeJS_Hall_Booking_API_Task🎊</h1>
      <ul>
         <li>Get all Rooms = endpoint: /rooms</li>
-        <li>Take a h1st of Booked rooms = endpoint: /rooms/customer</li>
+        <li>Take a list of Booked rooms = endpoint: /rooms/customer</li>
         <li>Create a new room = endpoint: /rooms/</li>
      </ul>
      </div>
